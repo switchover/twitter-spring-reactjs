af1e7ac65bd72f610363e85d48e232f392c5630c
Vincent Han <switchover@gmail.com>
2024-11-26 11:37:54 +0900

    Update TagMapperTest.java

--- tag-service/src/test/java/com/gmail/merikbest2015/mapper/TagMapperTest.java
+++ tag-service/src/test/java/com/gmail/merikbest2015/mapper/TagMapperTest.java
@@ -41,30 +41,13 @@ public class TagMapperTest {
 
     @Test
     public void getTags() {
-        when(tagService.getTags()).thenReturn(tags);
-        when(basicMapper.convertToResponseList(tags, TagResponse.class)).thenReturn(tagResponses);
-        assertEquals(2, tagMapper.getTags().size());
-        verify(tagService, times(1)).getTags();
-        verify(basicMapper, times(1)).convertToResponseList(tags, TagResponse.class);
     }
 
     @Test
     public void getTrends() {
-        PageRequest pageable = PageRequest.of(0, 20);
-        Page<Tag> pageableTags = new PageImpl<>(tags, pageable, 20);
-        HeaderResponse<TagResponse> headerResponse = new HeaderResponse<>(tagResponses, new HttpHeaders());
-        when(tagService.getTrends(pageable)).thenReturn(pageableTags);
-        when(basicMapper.getHeaderResponse(pageableTags, TagResponse.class)).thenReturn(headerResponse);
-        assertEquals(2, tagMapper.getTrends(pageable).getItems().size());
-        verify(tagService, times(1)).getTrends(pageable);
-        verify(basicMapper, times(1)).getHeaderResponse(pageableTags, TagResponse.class);
     }
 
     @Test
     public void getTweetsByTag() {
-        List<TweetResponse> tweetResponses = List.of(new TweetResponse(), new TweetResponse());
-        when(tagService.getTweetsByTag(TestConstants.HASHTAG)).thenReturn(tweetResponses);
-        assertEquals(tweetResponses, tagMapper.getTweetsByTag(TestConstants.HASHTAG));
-        verify(tagService, times(1)).getTweetsByTag(TestConstants.HASHTAG);
     }
 }
