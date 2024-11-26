6de1adc2ab92f6ef0ec0dbe39c1a4c7ac88744b7
Vincent Han <switchover@gmail.com>
2024-11-26 11:36:25 +0900

    Update TagResponse.java

--- tag-service/src/main/java/com/gmail/merikbest2015/dto/TagResponse.java
+++ tag-service/src/main/java/com/gmail/merikbest2015/dto/TagResponse.java
@@ -2,9 +2,6 @@ package com.gmail.merikbest2015.dto;
 
 import lombok.Data;
 
-@Data
 public class TagResponse {
-    private Long id;
-    private String tagName;
-    private Long tweetsQuantity;
+
 }
