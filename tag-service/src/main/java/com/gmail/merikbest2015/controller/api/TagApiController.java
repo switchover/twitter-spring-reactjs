55281f993dfcff9e82354671ce89be67419c98a6
Vincent Han <switchover@gmail.com>
2024-11-26 11:34:43 +0900

    Update TagApiController.java

--- tag-service/src/main/java/com/gmail/merikbest2015/controller/api/TagApiController.java
+++ tag-service/src/main/java/com/gmail/merikbest2015/controller/api/TagApiController.java
@@ -1,24 +1,4 @@
 package com.gmail.merikbest2015.controller.api;
 
-import com.gmail.merikbest2015.commons.constants.PathConstants;
-import com.gmail.merikbest2015.service.TagClientService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping(PathConstants.API_V1_TAGS)
 public class TagApiController {
-
-    private final TagClientService tagClientService;
-
-    @GetMapping(PathConstants.SEARCH_TEXT)
-    public List<String> getTagsByText(@PathVariable("text") String text) {
-        return tagClientService.getTagsByText(text);
-    }
 }
